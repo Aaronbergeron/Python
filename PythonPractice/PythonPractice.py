9cf32869b03c23c1bdfb35573a534ba371ec8193
Aaronbergeron <59907376+Aaronbergeron@users.noreply.github.com>
2020-03-07 14:56:10 -0500

    Add files via upload

---
+++ PythonPractice/PythonPractice.py
@@ -0,0 +1,19 @@
+x = 10
+y = 10                   #Implicit Declarations
+print(type(y))             #Prints type of variable y
+sum1 = x + x
+sum2 = y + y
+print(sum1, sum2)           
+print(sum1 + sum2)
+
+x = 30
+y = 35                      #3 ints, the total being assigned to int s
+z = 42
+s = x + y + z
+print(s)
+
+#Component datatypes
+student_grades = [9.1, 8.8, 7.5]                 #List
+
+
+
